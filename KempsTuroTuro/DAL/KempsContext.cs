07b2fade0597251e2ef37b20bb51900c5f6bd7f6
Brian Jill Sarandi <brian.sarandi@capgemini.com>
2015-06-17 10:51:53 +0200

    git ignoring

---
+++ KempsTuroTuro/DAL/KempsContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using KempsTuroTuro.Domain;
+
+namespace KempsTuroTuro.DAL
+{
+    public class KempsContext: DbContext
+    {
+        public KempsContext(): base("KempsContext")
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<PreparedItem> PreparedItems { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<PreparedItem>().Property(x => x.FoodItemHoldingTime).HasPrecision(4, 0);
+        }
+    }
+}
